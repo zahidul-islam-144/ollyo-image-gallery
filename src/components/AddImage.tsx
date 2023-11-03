e0aa9025c8b0b9b22c42e0f51dff07f47b866ba1
zahidul-islam-144 <z.islam.nishat@gmail.com>
2023-11-03 23:56:50 +0600

    file upload done

--- src/components/AddImage.tsx
+++ src/components/AddImage.tsx
@@ -1,30 +1,45 @@
-import React, { ChangeEvent, FC, FormEvent } from 'react';
+import React, { ChangeEvent, FC, FormEvent, useState } from "react";
 import { BiImageAdd } from "react-icons/bi";
-import { HTMLInputEvent } from '../utilities/types';
+import { HTMLInputEvent, ImageArrayType } from "../utilities/types";
+import useStore from "../hooks/useStore";
 
-const AddImage:FC = () => {
+const AddImage: FC = () => {
+  const { previewImageArray, setPreviewImageArray } = useStore();
+  const [newImageArray, setNewImageArray] = useState<ImageArrayType[]>([]);
 
-    const handleAddImage = (e:ChangeEvent<HTMLInputElement>) => {
-        const files = e.target?.files as FileList
-        const addedImageArray = Array.from(files) // converting FileList to File[]
-        console.log("* added:", addedImageArray)
-    }
-    return (
-        <div className='add_photo' >
-            <input
-                type="file"
-                multiple={true}
-                name="add-image"
-                id="_addImage"
-                accept='image/*'
-                onChange={(e)=>handleAddImage(e)}
-            />
-            <label className='label_block' htmlFor="_addImage">
-                <BiImageAdd className='add_icon'/>
-                Add Image
-            </label>
-        </div>
+  const handleAddImage = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target?.files as FileList;
+    // converting FileList to File[] using Array.from(obj) method
+    const addedImageArray = Array.from(files);
+    // updated adding newly image into existing array
+    addedImageArray?.forEach((newImg) =>
+      setPreviewImageArray((prev: any) => [
+        ...prev,
+        {
+          id: previewImageArray.length + 1,
+          image: URL.createObjectURL(newImg),
+        },
+      ])
     );
+  };
+
+
+  return (
+    <div className="add_photo">
+      <input
+        type="file"
+        multiple={true}
+        name="add-image"
+        id="_addImage"
+        accept="image/*"
+        onChange={(e) => handleAddImage(e)}
+      />
+      <label className="label_block" htmlFor="_addImage">
+        <BiImageAdd className="add_icon" />
+        Add Image
+      </label>
+    </div>
+  );
 };
 
 export default AddImage;
